8279fdc47d6042ff824cc02ab0028d9cdddf3ffd
abrahamayorinde <41977687+abrahamayorinde@users.noreply.github.com>
2022-10-04 22:20:28 -0400

    BASIC interpreter/compiler written in c++

---
+++ interpreter_compute.cpp
@@ -0,0 +1,552 @@
+#include <iostream>
+#include <string>
+
+//"MULTIPLY", "DIVIDE", "ADD", "SUBTRACT", "LPARENTH", "RPARENTH",
+using namespace std;
+
+bool isdigit(char digit);
+bool nearEnd(int place, int end);
+
+struct Token{
+    string Type;
+    int Value;
+};
+
+struct Node{
+    Token myNodeToken;
+    Node* left;
+    Node* right;
+    Node()
+    {
+        myNodeToken.Type = "";
+        myNodeToken.Value = 0;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Lexer{
+public:
+    string inputText;
+    int place;
+    int end;
+    Token thisToken;
+    int tokencount;
+    
+    void getInput(string text)
+    {
+        inputText = text;
+        place = 0;
+        end = inputText.length();
+        tokencount = 0;
+    }
+    
+    char currentSymbol()
+    {
+        if(place<end)
+        {
+            return inputText.at(place);
+        }
+        else
+        {
+            return inputText.at(end-1);
+        }
+    }
+    
+    Token getToken()
+    {
+        thisToken.Value = 0;
+        thisToken.Type = "";
+        string literalValue = "";
+        cout<<endl;
+        cout<<"getToken: place: "<<place<<endl;
+        if(place < end)
+        {
+            tokencount++;
+            cout<<"getToken: count: "<<tokencount<<endl;
+            while(isspace(inputText.at(place)) && place<end)
+            {
+                if(nearEnd(place,end))
+                {
+                    place++;
+                    goto labelout;
+                }
+                else
+                {
+                    place++;
+                }
+            }
+            if( isdigit(inputText.at(place)) and (place<end) )
+            {
+                while(isdigit(inputText.at(place)) and (place<end) )
+                {
+                    literalValue.append(1,inputText.at(place));
+                    if( place+1 >= end)
+                    {
+                        thisToken.Type = "INTEGER";
+                        thisToken.Value = stoi(literalValue);
+                        cout<<"getToken: value at end: "<<thisToken.Value<<endl;
+                        goto labelout;
+                    }
+                    thisToken.Value = stoi(literalValue);
+                    place++;
+                }
+                thisToken.Type="INTEGER";
+                thisToken.Value = stoi(literalValue);
+                cout<<"getToken: value before end: "<<thisToken.Value<<endl;
+                goto labelout;
+            }
+            if(inputText.at(place) == '+')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "ADD";
+                place++;
+                goto labelout;
+            }
+            if(inputText.at(place) == '-')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "SUBTRACT";
+                place++;
+                goto labelout;
+            }
+            if(inputText.at(place) == '*')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "MULTIPLY";
+                place++;
+                goto labelout;
+            }
+            if(inputText.at(place) == '/')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "DIVIDE";
+                place++;
+                goto labelout;
+            }
+            if(inputText.at(place) == '(')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "LPARENTH";
+                place++;
+                goto labelout;
+            }
+            if(inputText.at(place) == ')')
+            {
+                cout<<"getToken: "<<inputText.at(place)  <<endl;
+                thisToken.Type = "RPARENTH";
+                place++;
+                goto labelout;
+            }
+            labelout:
+            if(place == end)
+            {
+                cout<<"I've reached the end without knowing why!!!"<<endl;
+            }
+            else
+            {
+                cout<<"getToken: without issue count: "<<tokencount<<endl;
+                cout<<endl;
+            }
+            return thisToken;
+        }
+        else
+        {
+            return thisToken;
+        
+        }
+    }
+};
+
+class Parser
+{
+public:
+    /*"""term : factor ((MUL | DIV) factor)*"""*/
+    Lexer thislexer;
+    Token currentToken;
+    Node AST;
+    Node* astPointer;
+    string cursorTokenCharType;
+    int i;
+
+    Parser()
+    {
+        astPointer = &AST;
+    }
+    void getLexer(Lexer lexer)
+    {
+        thislexer = lexer;
+        i=0;
+    }
+     
+    void startLexer()
+    {
+        currentToken = thislexer.getToken();
+        cursorTokenCharType = getcursorSymbol();
+    }
+    
+    char getcursorSymbol()
+    {
+        return thislexer.currentSymbol();
+    }
+    
+    void eat(string tokenType)
+    {
+        if(currentToken.Type == tokenType)
+        {
+            cout<<"PARSER: EATEN AND DIGESTED: "<<currentToken.Type<<endl;
+            currentToken = thislexer.getToken();
+            cout<<endl;
+            cout<<"PARSER: JUST EATEN TOKEN : "<<currentToken.Type<<endl;
+        }
+        else
+        {
+            cout<<":Error:"<<endl;
+        }
+    }
+    
+    void traverse(Node* astTreePointer)
+    {
+        cout<<"TRAVERSE - Type: "<<astTreePointer->myNodeToken.Type<<endl;
+        cout<<"TRAVERSE - Value: "<<astTreePointer->myNodeToken.Value<<endl;
+        if (astTreePointer->left != NULL)
+        {
+            traverse(astTreePointer->left);
+        }
+        if (astTreePointer->right != NULL)
+        {
+            traverse(astTreePointer->right);
+        }
+    }
+
+    Node* factor()
+    {
+        int result;
+        Token current = currentToken;
+        
+        Node* astNode = new Node;
+
+        cout<<"PARSER: Factor Token Type: "<<currentToken.Type<<endl;
+
+        if( currentToken.Type == "INTEGER")
+        {
+            //
+            cout<<"PARSER: token #:"<<i+1<<endl;
+            cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+            cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+            i++;
+            //
+            astNode->myNodeToken.Value = currentToken.Value;
+            astNode->myNodeToken.Type = "INTEGER";
+            astNode->left = NULL;
+            astNode->right = NULL;
+            eat("INTEGER");
+
+            return astNode;
+        }
+        
+        if( currentToken.Type == "LPARENTH")
+        {
+            //
+            cout<<"PARSER: token #:"<<i+1<<endl;
+            cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+            cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+            cout<<endl;
+            i++;
+            //
+            eat("LPARENTH");
+            //astNode->myNodeToken.Type = "LPARENTH";
+            astNode->left = astNode;
+            astNode->right = expression();
+            astNode->myNodeToken.Type = currentToken.Type;
+            eat("RPARENTH");
+            return astNode;
+        }
+        
+        return astNode;
+    }
+    
+    Node* term()
+    {
+        Node* astNode = new Node;// factor();
+        Node* helpNode = new Node;
+        Token current = currentToken;
+        
+        //astNode->myNodeToken.Type = "NONE";
+        //astNode->Value = 0;
+        astNode->left = NULL;
+        astNode->right = NULL;
+        helpNode->left = NULL;
+        helpNode->right = NULL;
+        
+        astNode = factor();
+        
+        cout<<"PARSER: Term Token Type: "<<currentToken.Type<<endl;
+
+        while( (currentToken.Type == "MULTIPLY") || (currentToken.Type == "DIVIDE") )
+        {
+            if(currentToken.Type == "MULTIPLY")
+            {
+                //
+                cout<<"PARSER: token #:"<<i+1<<endl;
+                cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+                cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+                i++;
+                //
+                eat("MULTIPLY");
+                helpNode->left = astNode;
+                helpNode->myNodeToken.Type = "MULTIPLY";
+                helpNode->right = factor();
+                return helpNode;
+            }
+            if(currentToken.Type == "DIVIDE")
+            {
+                //
+                cout<<"PARSER: token #:"<<i+1<<endl;
+                cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+                cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+                i++;
+                //
+                eat("DIVIDE");
+                helpNode->left = astNode;
+                helpNode->myNodeToken.Type = "DIVIDE";
+                helpNode->right = factor();
+                return helpNode;
+            }
+            cout<<"Am I stuck here?"<<endl;
+        }
+        cout<<"PARSER, term, return: "<<astNode->myNodeToken.Type<<endl;
+        return astNode;
+    }
+    
+    Node* expression()
+    {
+        Node* astNode = new Node;
+        //Node* astPointer = new Node;
+        
+        astNode->myNodeToken.Type = "";
+        astNode->myNodeToken.Value = 0;
+        astNode->left = NULL;
+        astNode->right = NULL;
+        astPointer->left = NULL;
+        astPointer->right = NULL;
+        
+        astNode = term();
+        
+        cout<<"PARSER: Expression Token Type: "<<currentToken.Type<<endl;
+
+        while ((currentToken.Type == "ADD") || (currentToken.Type == "SUBTRACT"))
+        {
+            if(currentToken.Type == "ADD")
+            {
+                //
+                cout<<"PARSER: token #:"<<i+1<<endl;
+                cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+                cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+                i++;
+                //
+
+                astPointer->left = astNode;
+                astPointer->myNodeToken.Type = "ADD";
+                eat("ADD");
+                astPointer->right = term();
+                //AST = astPointer;
+                return astPointer;
+            }
+            if(currentToken.Type == "SUBTRACT")
+            {
+                //
+                cout<<"PARSER: token #:"<<i+1<<endl;
+                cout<<"PARSER: Token - Type: "<<currentToken.Type<<endl;
+                cout<<"PARSER: Token - Value: "<<currentToken.Value<<endl;
+                i++;
+                //
+                
+                astPointer->left = astNode;
+                astPointer->myNodeToken.Type = "SUBTRACT";
+
+                //
+                eat("SUBTRACT");
+                astPointer->right = term();
+                //AST = astPointer;
+                return astPointer;
+            }
+        }
+        
+        //AST = astPointer;
+        return astPointer;
+    }
+};
+
+class Interpreter
+{
+public:
+    /*"""term : factor ((MUL | DIV) factor)*"""*/
+    Lexer thislexer;
+    Token currentToken;
+    string cursorTokenCharType;
+
+    void getLexer(Lexer lexer)
+    {
+        thislexer = lexer;
+    }
+     
+    void startLexer()
+    {
+        currentToken = thislexer.getToken();
+        cursorTokenCharType = getcursorSymbol();
+    }
+    
+    char getcursorSymbol()
+    {
+        return thislexer.currentSymbol();
+    }
+    
+    void eat(string tokenType)
+    {
+        Token current = currentToken;
+        if(currentToken.Type == tokenType)
+        {
+            currentToken = thislexer.getToken();
+        }
+        else
+        {
+            cout<<":Error:"<<endl;
+        }
+    }
+
+    int factor()
+    {
+        int result;
+        Node currentNode;
+        Token current = currentToken;
+        
+        if( currentToken.Type == "INTEGER")
+        {
+            eat("INTEGER");
+            return current.Value;
+        
+        }
+        else if( currentToken.Type == "LPARENTH")
+        {
+            eat("LPARENTH");
+            result = expression();
+            eat("RPARENTH");
+            return result;
+        }
+        return result;
+    }
+    
+    int term()
+    {
+        int result = factor();
+        Token current = currentToken;
+        
+        cout<<"Term Token Type: "<<currentToken.Type<<endl;
+        while( (currentToken.Type == "MULTIPLY") || (currentToken.Type == "DIVIDE") )
+        {
+            if(currentToken.Type == "MULTIPLY")
+            {
+                eat("MULTIPLY");
+                result = result * factor();
+            }
+            if(currentToken.Type == "DIVIDE")
+            {
+                eat("DIVIDE");
+                result = result / factor();
+            }
+        }
+        
+        return result;
+    }
+    
+    int expression()
+    {
+        int result = term();
+        
+        while ((currentToken.Type == "ADD") || (currentToken.Type == "SUBTRACT"))
+        {
+            if(currentToken.Type == "ADD")
+            {
+                eat("ADD");
+                result = result + term();
+            }
+            if(currentToken.Type == "SUBTRACT")
+            {
+                eat("SUBTRACT");
+                result = result - term();
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    string prompt = "Enter an expression to be evalauted:\n";
+    string answer;
+    //Token foundToken;
+    //Token currentToken;
+    Lexer thisLexer;
+    Lexer thisLexer2;
+    
+    Interpreter interpreter;
+    Parser parser;
+    
+    Node* testNode;
+    
+    cout<<prompt;
+    
+    getline(cin,answer);
+    
+    //thisLexer.getInput(answer);
+    thisLexer2.getInput(answer);
+    
+    //interpreter.getLexer(thisLexer);
+    
+    //interpreter.startLexer();
+    
+    //cout<<interpreter.expression()<<endl;
+
+    parser.getLexer(thisLexer2);
+    
+    parser.startLexer();
+    
+    testNode =  parser.expression();
+    
+    //parser.traverse(testNode);
+    
+    //cout<<"Type of the test node: "<<testNode->myNodeToken.Type<<endl;
+    //cout<<"Value of the test node: "<<testNode->myNodeToken.Value<<endl;
+    
+    //cout<<"Type of left node of the test node: "<<testNode->left->myNodeToken.Type<<endl;
+    //cout<<"Value of left node of the test node: "<<testNode->left->myNodeToken.Value<<endl;
+
+    //cout<<"Type of right node of the test node: "<<testNode->right->myNodeToken.Type<<endl;
+    //cout<<"Value of right node of the test node: "<<testNode->right->myNodeToken.Value<<endl;
+     
+    return 0;
+}
+
+
+bool isdigit(char digit)
+{
+    if(digit == '1' || digit == '2' || digit == '3' || digit == '4' || digit == '5' || digit == '6' || digit == '7' || digit == '8' || digit == '9' || digit == '0')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool nearEnd(int place, int end)
+{
+    if( (place+1) >= end)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
